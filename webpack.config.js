55fe0791ea52c80a8a4736937fb475308c4ccc10
artryan130 <80287857+artryan130@users.noreply.github.com>
2022-03-07 11:03:17 -0300

    Plugins HTML Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
 
 module.exports ={
     mode: 'development',
@@ -12,6 +14,12 @@ module.exports ={
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
 
     module: {
         rules: [
